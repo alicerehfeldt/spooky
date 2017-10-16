09b91666c07b95f4b8f1facfa4723ba497afd25d
alice <gothiclolita@gmail.com>
2017-10-15 17:04:00 -0700

    photo index

--- photos/index.php
+++ photos/index.php
@@ -1,5 +1,12 @@
 <?php
 
+$images = new Array();
+$files = scandir('.', SCANDIR_SORT_DESCENDING);
+foreach ($files as $file) {
+  if (preg_match('@\.jpg$@', $file)) {
+    array_push($images, $file);
+  }
+}
 
 
 ?>
@@ -8,4 +15,17 @@
     <head>
         <title>Alice's Spooky Party</title>
     </head>
+    <body>
+      <?php
+        foreach($images as $image) {
+          echo <<<HTML
+        <div class="photo">
+          <img src="{$image}" />
+          <span>$image</span>
+        </div>
+HTML;
+        }
+      ?>
+    </body>
+
 </html>
